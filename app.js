e9acc22729f14f65a4502359cc9a400e18fe6a72
cloudblaze123 <cloudblaze123@gmail.com>
2024-06-13 15:08:02 +0800

    主要变化：在返回"Hello, World"的同时，在后台打印访问的用户浏览器信息。

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// 中间件：打印访问用户的信息
+app.use((req, res, next) => {
+    const { method, url, headers } = req;
+    console.log(`Request received: Method=${method}, URL=${url}, User-Agent=${headers['user-agent']}`);
+    next(); // 继续处理请求
+});
+
 // 定义一个路由处理根路径请求，返回 "Hello, World!"
 app.get('/', (req, res) => {
     res.send('Hello, World!');
