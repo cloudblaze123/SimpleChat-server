b73e175e51798d50f235968074e5b07ef377012a
cloudblaze123 <cloudblaze123@gmail.com>
2024-06-15 16:55:30 +0800

    主要变化：为express配置静态文件服务，将index的html代码转移到public文件夹中

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
+
+
+// 配置静态文件服务
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // 中间件：打印访问用户的信息
 app.use((req, res, next) => {
@@ -9,40 +16,12 @@ app.use((req, res, next) => {
     next(); // 继续处理请求
 });
 
+
 // 定义一个路由，返回 "Hello, World!"
 app.get('/api/hello', (req, res) => {
     res.send('Hello, World!');
 });
 
-// 定义一个路由处理根路径请求，返回页面代码
-app.get('/', (req, res) => {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Fetch Example</title>
-        <script>
-            async function fetchMessage() {
-                try {
-                    const response = await fetch('http://localhost:3000/api/hello');
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    const message = await response.text();
-                    document.getElementById('message').innerText = message;
-                } catch (error) {
-                    console.error('There has been a problem with your fetch operation:', error);
-                }
-            }
-        </script>
-    </head>
-    <body>
-        <button onclick="fetchMessage()">Get Message</button>
-        <div id="message"></div>
-    </body>
-    </html>`);
-});
 
 // 让服务器监听指定端口
 app.listen(port, () => {
