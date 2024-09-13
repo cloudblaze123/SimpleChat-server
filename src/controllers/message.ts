44b76a08044fe0afba82f8a718bd581479519d9b
cloudblaze123 <cloudblaze123@gmail.com>
2024-09-13 18:04:57 +0800

    小更新：bug 缓解，延迟服务器在收到消息后通知客户端更新消息的过程，以减轻客户端在增量更新消息时会获取已收到过的消息的问题
    bug来源：
    在依赖于 api v1.1 的前端项目中，当客户端时间落后于服务器时间时，客户端传递给 '/api/messages/:date' 接口 date 参数可能会晚于服务器上次通知客户端更新消息的时间，导致客户端重复获取上次服务器通知获取的消息。

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -9,16 +9,20 @@ function initMessageHandler() {
     messageStore.on("messageAdded", (message) => {
         const { senderId, receiverId} = message;
         
-        // 使用 socket 通知用户更新数据
-        // console.log(socketStore)
-        if (socketStore[senderId]) {
-            console.log(`user ${senderId} online, emit messageAdded`);
-            socketStore[senderId].emit("messageAdded");
-        }
-        if (socketStore[receiverId]) {
-            console.log(`user ${receiverId} online, emit messageAdded`);
-            socketStore[receiverId].emit("messageAdded");
-        }
+        // 延迟 1s 后通知用户更新数据
+        // 减少因客户端时间落后与服务器时间导致客户端请求的消息重影的问题
+        setTimeout(() => {
+            // 使用 socket 通知用户更新数据
+            // console.log(socketStore)
+            if (socketStore[senderId]) {
+                console.log(`user ${senderId} online, emit messageAdded`);
+                socketStore[senderId].emit("messageAdded");
+            }
+            if (socketStore[receiverId]) {
+                console.log(`user ${receiverId} online, emit messageAdded`);
+                socketStore[receiverId].emit("messageAdded");
+            }
+        }, 1000);
     });
 }
 
