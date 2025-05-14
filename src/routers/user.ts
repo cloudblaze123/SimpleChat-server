85102be12f71570735234d052de98efb2d3a13ba
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-14 11:28:58 +0800

    小更新：api 更新至v2.1，新增【更新用户资料】接口

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -41,10 +41,28 @@ router.get('/api/user/:id', (req, res) => {
 });
 
 
-router.get('/api/usera/router', (req, res) => {
 
-    res.send('user router');
 
+// 修改信息
+router.put('/api/user/:id/profile', (req, res) => {
+    const userId = req.params.id; // 从请求参数中获取用户ID并转换为整数
+    const user = users.find(u => u.id === userId)
+    const { profile } = req.body
+
+    if (!user) {
+        res.status(404).json({ message: 'User not found' }); // 如果没有找到用户，返回404错误
+        return;
+    }
+
+    console.log(`User ${userId} is trying to update profile`); // 打印日志
+
+    for(const key of Object.keys(profile)) {
+        if (key in user){
+            (user as any)[key] = profile[key];
+        }
+    }
+
+    res.status(200).json({ message: 'Profile updated successfully' }); // 返回成功信息
 });
 
 
