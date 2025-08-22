6d41c713e7dc6fce2c6a9d54b12ae14191644c5f
cloudblaze123 <cloudblaze123@gmail.com>
2025-08-22 23:17:23 +0800

    小更新：安装express

--- main.ts
+++ main.ts
@@ -1,1 +1,38 @@
-console.log('hello typescript')
+import express from 'express';
+
+
+
+const app = express();
+const port = 3000;
+
+
+
+
+// 配置中间件
+import { Request, Response, NextFunction } from 'express';
+
+
+// 中间件：打印访客信息
+const logUrlAccessInfoMiddleware = (req:Request, res:Response, next:NextFunction) => {
+    const { method, url } = req;
+    console.log(`Request received: Method=${method}, URL=${url}`);
+    next(); // 继续处理请求
+}
+app.use(logUrlAccessInfoMiddleware);
+
+
+
+
+// 配置路由
+
+// 用来测试的路由
+app.get('/api/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
